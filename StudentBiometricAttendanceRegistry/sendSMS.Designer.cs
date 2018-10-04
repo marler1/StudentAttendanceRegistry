c05fab32a3625db85dac0abc4bc1cdd824943e3c
marler1 <edetricky@gmail.com>
2018-10-04 15:06:33 +0300

    SMS functionality

--- StudentBiometricAttendanceRegistry/sendSMS.Designer.cs
+++ StudentBiometricAttendanceRegistry/sendSMS.Designer.cs
@@ -41,7 +41,8 @@
             // 
             // btnsend
             // 
-            this.btnsend.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnsend.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnsend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnsend.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnsend.ForeColor = System.Drawing.Color.White;
             this.btnsend.Location = new System.Drawing.Point(430, 436);
